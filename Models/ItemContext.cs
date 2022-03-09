ac68dc0397483dacafee8e8d2dc74d9fa25b0b4f
Marcello FABBRI <marcello.fabbri@alten.it>
2022-03-09 12:39:24 +0100

    First Commit test git 2

---
+++ Models/ItemContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ItemApi.Models
+{
+    public class ItemContext : DbContext
+    {
+        public ItemContext(DbContextOptions<ItemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; } = null!;
+    }
+}
